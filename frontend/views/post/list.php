9bebe82f2456a63a45313e2a748fa69115344cf1
Fayoz-dev <frasulov988@gmail.com>
2024-04-03 12:43:02 +0500

    alias && helpers function

--- frontend/views/post/list.php
+++ frontend/views/post/list.php
@@ -1,1 +1,15 @@
-<?php echo "bu post list";
+<?php
+
+use yii\helpers\Html;
+
+echo Html::tag('h1', Html::encode('hunter'), ['class'=> 'bu-username']);
+
+echo Html::beginForm(['order/update', 'id' => 12], 'post', ['enctype'=>'multipart/form-data']);
+
+echo Html::input('text', 'username', 'hunter', ["class" => "class-hunter"]);
+
+echo Html::submitButton('Submit', ['class'=> 'submit']);
+
+echo Html::endForm();
+
+echo Html::checkbox('agree', true , ['label' => 'i agree']);
